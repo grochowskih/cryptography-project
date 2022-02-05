d64eaf3fca2e38ab120c133ce3f11bcd74bd85f6
bezuu <63921655+bezuu@users.noreply.github.com>
2022-02-05 16:09:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import generators.generator
 import DSA.dsa
 import random
+import TDES.OFB
 
 if __name__ == "__main__":
     used_ks = []
     while True:
         print("----- 3DES OFB + DSA-SHA-384 -----")
-        print("A. Wygeneruj podpis cyfrowy \nB. Zweryfikuj podpis cyfrowy")
+        print("A. Wygeneruj podpis cyfrowy \nB. Zweryfikuj podpis cyfrowy \nC. Zaszyfruj wiadomość \nD. Odszyfruj wiadomość ")
         action = input("Podaj opcję: ")
         if action == "A":
             choice = input(
@@ -72,8 +73,148 @@ if __name__ == "__main__":
                 print("Czy podpis jest poprawny? ", correct)
             except:
                 print("Przechwyciłem wyjątek!")
+        elif action == "C":
+            plaintext = input("Podaj tekst, który chcesz zaszyfrować: ")
+            choice1 = input("Jeśli chcesz sam podać klucze i wektor inicjalizujący, wciśnij 1. Jeśli nie, wciśnij "
+                            "cokolwiek innggo: ")
+            if choice1 == '1':
+                Iv = input("Podaj wektor inicjalizujący: ")
+                while len(TDES.OFB.hex_to_binary(Iv.encode("utf-8").hex())) != 64:
+                    Iv = input("Zła długość wektora inicjalizującego. Podaj nowy wektor inicjalizujący: ")
+                print("Wybierz opcję kluczy. \n 1. Wszystkie klucze są różne i od siebie niezależne. "
+                      "\n 2. Klucz pierwszy i klucz drugi są od siebie niezależne oraz klucz pierwszy jest taki sam jak klucz"
+                      "trzeci. \n 3. Wszystkie klucze są takie same (TDES trywializuje się do DES)")
+                choice = input("Wybierz 1, 2 lub 3: ")
+                if choice == "1":
+                    key1 = input("Podaj pierwszy klucz: ")
+                    key2 = input("Podaj drugi klucz: ")
+                    key3 = input("Podaj trzeci klucz: ")
 
+                    while key1 == key2 or key1 == key3 or key2 == key3:
+                        print("Klucze się powtarzają. Wybierz nowe klucze.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+                        key3 = input("Podaj trzeci klucz: ")
 
+                    while len(TDES.OFB.hex_to_binary(key1.encode("utf-8").hex())) != 64 or \
+                          len(TDES.OFB.hex_to_binary(key2.encode("utf-8").hex())) != 64 or \
+                          len(TDES.OFB.hex_to_binary(key3.encode("utf-8").hex())) != 64:
+                        print("Długość kluczy jest zła.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+                        key3 = input("Podaj trzeci klucz: ")
+
+                elif choice == "2":
+                    key1 = input("Podaj pierwszy klucz: ")
+                    key2 = input("Podaj drugi klucz: ")
+
+                    while key1 == key2:
+                        print("Klucze się powtarzają. Wybierz nowe klucze.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+
+                    while len(TDES.OFB.hex_to_binary(key1.encode("utf-8").hex())) != 64 or \
+                          len(TDES.OFB.hex_to_binary(key2.encode("utf-8").hex())) != 64:
+                        print("Długość kluczy jest zła.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+                    key3 = key1
+
+                elif choice == "3":
+                    key1 = input("Podaj klucz: ")
+
+                    while len(TDES.OFB.hex_to_binary(key1.encode("utf-8").hex())) != 64:
+                        key1 = input("Zła długość klucza. Podaj nowy klucz: ")
+                    key2 = key1
+                    key3 = key1
+                key1 = key1.encode("utf-8").hex()
+                key2 = key2.encode("utf-8").hex()
+                key3 = key3.encode("utf-8").hex()
+                Iv = Iv.encode("utf-8").hex()
+                plaintext = plaintext.encode("utf-8").hex()
+                print("Oto Twoja zaszyfrowana wiadomość: ", TDES.OFB.ofb_encrypt(plaintext,key1,key2,key3,Iv))
+
+            else:
+                key1 = TDES.OFB.generate_iv()
+                key2 = TDES.OFB.generate_iv()
+                key3 = TDES.OFB.generate_iv()
+                Iv = TDES.OFB.generate_iv()
+                plaintext = plaintext.encode("utf-8").hex()
+                print("Oto Twoja zaszyfrowana wiadomość: ", TDES.OFB.ofb_encrypt(plaintext, key1, key2, key3, Iv))
+
+        elif action == "D":
+            ciphertext = input("Podaj tekst, który chcesz odszyfrować: ")
+            choice2 = input("Jeśli twój tekst jest już w systemie szesnastkowym, wciśnij 1. Jeśli nie, wciśnij "
+                            "cokolwiek innego: ")
+            choice1 = input("Jeśli chcesz sam podać klucze i wektor inicjalizujący, wciśnij 1. Jeśli nie, wciśnij "
+                            "cokolwiek innggo: ")
+            if choice1 == '1':
+                Iv = input("Podaj wektor inicjalizujący 64-bitowy: ")
+                while len(TDES.OFB.hex_to_binary(Iv.encode("utf-8").hex())) != 64:
+                    Iv = input("Zła długość wektora inicjalizującego. Podaj nowy wektor inicjalizujący: ")
+                print("Wybierz opcję kluczy. Niezależnie od wyboru, klucze muszą być 64-bitowe."
+                      " \n 1. Wszystkie klucze są różne i od siebie niezależne. "
+                      "\n 2. Klucz pierwszy i klucz drugi są od siebie niezależne oraz klucz pierwszy jest taki sam "
+                      "jak klucz trzeci. \n 3. Wszystkie klucze są takie same (TDES trywializuje się do DES)")
+                choice = input("Wybierz 1, 2 lub 3: ")
+                if choice == "1":
+                    key1 = input("Podaj pierwszy klucz: ")
+                    key2 = input("Podaj drugi klucz: ")
+                    key3 = input("Podaj trzeci klucz: ")
+
+                    while key1 == key2 or key1 == key3 or key2 == key3:
+                        print("Klucze się powtarzają. Wybierz nowe klucze.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+                        key3 = input("Podaj trzeci klucz: ")
+
+                    while len(TDES.OFB.hex_to_binary(key1.encode("utf-8").hex())) != 64 or \
+                            len(TDES.OFB.hex_to_binary(key2.encode("utf-8").hex())) != 64 or \
+                            len(TDES.OFB.hex_to_binary(key3.encode("utf-8").hex())) != 64:
+                        print("Długość kluczy jest zła.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+                        key3 = input("Podaj trzeci klucz: ")
+
+                elif choice == "2":
+                    key1 = input("Podaj pierwszy klucz: ")
+                    key2 = input("Podaj drugi klucz: ")
+
+                    while key1 == key2:
+                        print("Klucze się powtarzają. Wybierz nowe klucze.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+
+                    while len(TDES.OFB.hex_to_binary(key1.encode("utf-8").hex())) != 64 or \
+                            len(TDES.OFB.hex_to_binary(key2.encode("utf-8").hex())) != 64:
+                        print("Długość kluczy jest zła.")
+                        key1 = input("Podaj pierwszy klucz: ")
+                        key2 = input("Podaj drugi klucz: ")
+                    key3 = key1
+
+                elif choice == "3":
+                    key1 = input("Podaj klucz: ")
+
+                    while len(TDES.OFB.hex_to_binary(key1.encode("utf-8").hex())) != 64:
+                        key1 = input("Zła długość klucza. Podaj nowy klucz: ")
+                    key2 = key1
+                    key3 = key1
+                key1 = key1.encode("utf-8").hex()
+                key2 = key2.encode("utf-8").hex()
+                key3 = key3.encode("utf-8").hex()
+                Iv = Iv.encode("utf-8").hex()
+                if choice2 != '1':
+                    ciphertext = ciphertext.encode("utf-8").hex()
+                print("Oto Twoja odszyfrowana wiadomość: ", TDES.OFB.ofb_decrypt(ciphertext, key1, key2, key3, Iv))
+
+            else:
+                key1 = TDES.OFB.generate_iv()
+                key2 = TDES.OFB.generate_iv()
+                key3 = TDES.OFB.generate_iv()
+                Iv = TDES.OFB.generate_iv()
+                if choice2 != '1':
+                    ciphertext = ciphertext.encode("utf-8").hex()
+                print("Oto Twoja odszyfrowana wiadomość: ", TDES.OFB.ofb_decrypt(ciphertext, key1, key2, key3, Iv))
 
 
 
